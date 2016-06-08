1ba103d73bfcca1f44255f3e84d50300d1fa9c4a
felipefagua <felipefagua@gmail.com>
2016-06-08 15:14:58 -0500

    GUI Fixed to work by ratio and not by pixel
    The in game UI components were fixed.  Now they work by setting the
    ratio of the screen and not the position and size by the amount of
    pixels.

---
+++ Santman/Assets/Scripts/ui/ScalableRatioUIComponent.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScalableRatioUIComponent : MonoBehaviour {
+
+    [SerializeField]
+    private Rect _ratioSettings;
+
+    private Rect _screenSettings;
+
+    [SerializeField]
+    private bool _updateOnUpdate;
+
+    private RectTransform _myTransform;
+
+	// Use this for initialization
+	void Start () {
+        _myTransform = GetComponent<RectTransform>();
+        UpdateUIComponent();
+	}
+	
+    // Update is called once per frame
+	void Update () {
+	    if (_updateOnUpdate)
+            UpdateUIComponent();
+    }
+
+    private void UpdateUIComponent() {
+        UpdateScreenSettings();
+        UpdateComponentSize();
+        UpdateComponentPosition();
+    }
+
+    private void UpdateScreenSettings() {
+        RectTransform canvasTransform = (RectTransform)FindObjectOfType<Canvas>().transform;
+        _screenSettings.width = canvasTransform.rect.width;
+        _screenSettings.height = canvasTransform.rect.height;
+    }
+
+    private void UpdateComponentSize() {
+        float width = (_screenSettings.width * _ratioSettings.width);
+        float height = (_screenSettings.height * _ratioSettings.height);
+        Vector2 size = new Vector2(width, height);
+        _myTransform.sizeDelta = size;
+    }
+
+    private void UpdateComponentPosition() {
+        float x = (_screenSettings.width * _ratioSettings.x);
+        float y = (_screenSettings.height * _ratioSettings.y);
+        Vector2 position = new Vector2(x, y);
+        _myTransform.anchoredPosition = position;
+    }
+}
